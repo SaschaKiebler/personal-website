8159d5f42eb7211bc5906150cc93880d19284014
sascha <saschakiebler@gmail.com>
2023-01-15 16:28:06 +0100

    zum test 3

--- components/Nav.js
+++ components/Nav.js
@@ -1,28 +1,35 @@
 import Link from "next/link";
 import {auth} from "../utils/firebase";
 import {useAuthState} from "react-firebase-hooks/auth";
+import {FaCrown} from "react-icons/fa";
+
 
 export default function Nav(){
     const [user,loading] = useAuthState(auth);
 return(
-    <nav id="nav">
+    <nav className="w-full">
+        <ul className="inline-flex bg-black text-white w-full p-4 mb-4">
+        <li>
         <Link href="/">
-        <button className="navItem">Sascha Kiebler</button>
-        </Link>
-        <ul>
+        <button className="mr-1">Sascha Kiebler</button> 
+        </Link></li>
+        <li className="mr-4 center"><FaCrown/></li>
         {!user && (
-            <Link href="/auth/login">
+            <li><Link href="/auth/login">
             <p>Login</p>
-            </Link>
+            </Link></li>
         )}
         {user && ( 
         <div>
+            <li>
                 <Link href="/post">
                 <button>Post</button>
                 </Link>
+                </li>
+                <li>
                 <Link href="/dashboard">
-                <img id="avatarNav" src={user.photoURL}/>
-                </Link>
+                <img  src={user.photoURL}/>
+                </Link></li>
         </div>
         )}
         </ul>
